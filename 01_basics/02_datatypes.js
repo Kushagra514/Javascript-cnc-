efe4d7afd5c7e96b2d2f1ccee6fde308cfae1567
Kushagra514 <kushagratewari.eng@gmail.com>
2025-11-18 06:37:59 +0000

    added datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) //we are using nodejs,not browser
+
+console.log(3+3)
+console.log("Kushagra")
+
+let name="Kushagra"
+let age=19
+let isloggedin=false
+
+//number -> two to the power 53
+//bigint
+//string -> " "
+//boolean -> true or false
+//null -> standalone value
+//undefined -> value not yet assigned
+//symbol -> unique 
+
+//object 
+
+console.log(typeof null)// object
+console.log(typeof undefined) //undefined
